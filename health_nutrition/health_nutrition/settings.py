53efecd62050f39114fb44142066986f169e3c11
unknown <karolzalecki@gmail.com>
2022-02-27 19:53:31 +0100

    add chart and table

--- health_nutrition/health_nutrition/settings.py
+++ health_nutrition/health_nutrition/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- health_nutrition/results/views.py
+++ health_nutrition/results/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views import View
 from django.http import request
 import pandas as pd
-import seaborn as sn
+import seaborn as sns
 import numpy as np
-import matplotlib as plt
-
+import matplotlib.pyplot as plt
+from pandas.plotting import table
+import dataframe_image as dfi
 
 class ShowMenu(View):
     """class based view showing main menu of analysis"""
@@ -25,7 +26,39 @@ class ShowBMIAnalysis(View):
     """class based view showing results of BMI analysis"""
 
 
+
     def get(self, request):
         """class method,
         result: show html with result of BMI analysis"""
-        return render(request, 'bmi_analysis.html')
+        bmi = pd.read_csv('results\data_files\hlth_ehis_bm1e_1_Data.csv')
+        bmi_head = bmi.head(20) # data input as data frame
+        df_styled = bmi_head.style.background_gradient()
+        dfi.export(df_styled, "bmi_table.png")
+        unique_geo_data = bmi['GEO'].unique()
+
+        bmi_columns = bmi.columns
+        bmi_types = bmi['BMI'].unique()
+        bmi_description = bmi.describe(include=['O'])
+        df_styled_2 = bmi_description.style.background_gradient()
+        dfi.export(df_styled_2,'static\description.png')
+        # plot = plt.subplot(111, frame_on=False)
+        # plot.xaxis.set_visible(False)
+        # plot.yaxis.set_visible(False)
+        # table(plot, bmi_description, loc='upper right')
+        # pict = plt.savefig('desc_plot.png')
+        first_conclusions = "First conclusions: data frame contains 1224 records. There are 3 BMI values: Underweight, " \
+                            "Normal and Overweigth. Data covers 34 countries (including collected information for the EU), " \
+                            "3 education levels, provides differentiation between man and women, but not the age. " \
+                            "Therefore the column: AGE can also be removed."
+        heat_plot = sns.heatmap(bmi.isnull())
+        heat_plot_pict = heat_plot.get_figure()
+        heat_plot_pict.savefig("output.png")
+
+
+        return render(request, 'bmi_analysis.html', {'bmi': bmi,
+                                                         'geo': unique_geo_data,
+                                                         'columns': bmi_columns,
+                                                         'bmi_types': bmi_types,
+                                                         'bmi_description': bmi_description,
+                                                        'first_conclusions':first_conclusions,
+                                                        })
